da6dbe508d540b96582cc42a0c53c184167335a2
michaelangelo-git <michaellostrittojr@gmail.com>
2021-04-29 23:24:27 -0400

    fixed a form submission issue

--- script.js
+++ script.js
@@ -311,9 +311,10 @@ function fadeAndRemember(targetClassName, startOpacity, endOpacity, delay) {
 /***************************************************************************************************/
 /***************************FUNCTION TESTING AREA **************************************************/
 
-let sessionSubmit = document.getElementById('sessionSubmit');
+let sessionSubmit = document.querySelector('.choose-sessions');
 //setTimeout(fadeAndRemember('.choose-sessions', 0, 1, 1500), 2000);
 sessionSubmit.addEventListener('submit', function(){
+    
     boxBreathingAnimation(document.getElementById('numSessions').value);
     changeBreathingInstructions(document.getElementById('numSessions').value);
     //set timeout and call instructional text animation
